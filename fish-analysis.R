973fc74ee0b965c6f6f529d8226202757a8c7de6
Katherine Graham <kgraham5@unl.edu>
2018-10-19 11:33:37 -0500

    changed big from 200 to 300

--- fish-analysis.R
+++ fish-analysis.R
@@ -5,5 +5,5 @@ library(tidyverse)
 fishdata <- read_csv("Data/Gaeta_etal_CLC_data.csv")
 
 fishdata %>% 
-  mutate(length_cut = case_when(length >= 200 ~ "big",
+  mutate(length_cut = case_when(length >= 300 ~ "big",
                                 length < 200 ~ "small")) -> fishdata
