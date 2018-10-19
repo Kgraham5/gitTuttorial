58baa0dddf024af85a78d8d2001f8e349363408e
Katherine Graham <kgraham5@unl.edu>
2018-10-19 11:31:01 -0500

    fixed error

---
+++ fish-analysis.R
@@ -0,0 +1,9 @@
+
+.libPaths("P://RLibs")
+library(tidyverse)
+
+fishdata <- read_csv("Data/Gaeta_etal_CLC_data.csv")
+
+fishdata %>% 
+  mutate(length_cut = case_when(length >= 200 ~ "big",
+                                length < 200 ~ "small")) -> fishdata
