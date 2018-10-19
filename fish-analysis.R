922b6c3f4b5e42c458f37de9f735ffd464671352
Katherine Graham <kgraham5@unl.edu>
2018-10-19 11:47:15 -0500

    scalelength histogram

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,9 +1,20 @@
 
 .libPaths("P://RLibs")
 library(tidyverse)
+source("R/themes.R")
+
+library(extrafont)
+loadfonts()
 
 fishdata <- read_csv("Data/Gaeta_etal_CLC_data.csv")
 
 fishdata %>% 
-  mutate(length_cut = case_when(length >= 300 ~ "big",
+  mutate(length_cut = case_when(length >= 200 ~ "big",
                                 length < 200 ~ "small")) -> fishdata
+
+ggplot(fishdata) +
+  geom_histogram(aes(x = scalelength, fill = length_cut))+
+  facet_wrap(~length_cut) +
+  theme_presentation()
+
+
